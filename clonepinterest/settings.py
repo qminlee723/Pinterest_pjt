2bda561f955d617ee7519791faab9ce7f023ad91
Gyumin Lee <gyumin.q.lee@gmail.com>
2022-04-22 00:42:39 +0900

    add css

--- clonepinterest/settings.py
+++ clonepinterest/settings.py
@@ -134,7 +134,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-fieldSTATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
