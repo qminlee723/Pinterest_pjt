ff89ce24e500cd6db721d750f36229d9f2c30e61
Gyumin Lee <gyumin.q.lee@gmail.com>
2022-04-22 01:51:15 +0900

    update views and html - GET and POST

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == 'POST':
+        context = {'text': 'POST METHOD!!!'}
+        return render(request, 'accountapp/hello_world.html', context)
+    else:
+        context = {'text': 'GET METHOD!!!'}
+        return render(request, 'accountapp/hello_world.html', context)
